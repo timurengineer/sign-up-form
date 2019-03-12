87ed9c8d93fe14ea423f8cf5a9e809e5904aa118
Timur Z <engineer@timurz.com>
2019-03-12 14:42:34 +0600

    add form with TextInputs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { css } from 'emotion';
 import './App.css';
+import TextInput from './TextInput';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+const title = {
+  textAlign: 'center',
+}
+
+const form = {
+  padding: '0 12px',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  maxWidth: '500px',
+}
+
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirm, setConfirm] = useState('');
+
+  const onFormSubmit = (event) => {
+    event.preventDefault()
+    console.log('submit!')
+  };
+
+  return (
+    <div className="App">
+      <h1 className={css(title)}>Sign Up</h1>
+      <form onSubmit={onFormSubmit} className={css(form)}>
+        <TextInput
+          name="username"
+          id="username"
+          label="Username"
+          value={username}
+          onChange={event => setUsername(event.target.value)}
+          autoFocus
+        />
+        <TextInput
+          type="password"
+          id="password"
+          name="password"
+          label="Password"
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+        <TextInput
+          type="password"
+          id="confirm"
+          name="confirm"
+          label="Confirm Password"
+          value={confirm}
+          onChange={event => setConfirm(event.target.value)}
+        />
+      </form>
+    </div>
+  )
 }
 
 export default App;
