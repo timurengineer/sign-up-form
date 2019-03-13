fab033637c50d4ee7f7032a0c46a8d0d76b83049
Timur Z <engineer@timurz.com>
2019-03-13 15:12:37 +0600

    add front end validations to api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
 const { User } = require('./db');
+const {
+  validate,
+  usernameValidators,
+  passwordValidators
+} = require('../src/api');
 
 const app = express();
 const port = 3001;
@@ -8,6 +13,8 @@ const port = 3001;
 app.use(express.json())
 
 app.post('/users', (req, res) => {
+  const usernameError = validate(req.body.username, usernameValidators);
+  const passwordError = validate(req.body.password, passwordValidators);
   const user = new User();
 
   user.username = req.body.username;
@@ -16,6 +23,26 @@ app.post('/users', (req, res) => {
   user.save((error, newUser) => {
     if (error) {
       return res.status(400).json(error);
+    }
+
+    if (usernameError) {
+      return res.status(400).json({
+        name: 'ValidationError',
+        username: {
+          message: usernameError,
+        },
+        message: usernameError,
+      })
+    }
+  
+    if (passwordError) {
+      return res.status(400).json({
+        name: 'ValidationError',
+        password: {
+          message: passwordError,
+        },
+        message: passwordError,
+      })
     }
 
     res.json(newUser);
